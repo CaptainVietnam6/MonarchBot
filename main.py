95b511ecf631332917f5d2038ce5da08c1f8f24e
Kiet Pham <>
2021-04-23 18:59:02 +0000

    Added license

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+'''
+Copyright (©) 2021 Kiet Pham <kiet.riley2005@gmail.com>
+This software/program has a copyright license, more information is in the 'LICENSE' file
+IF YOU WANT TO USE/COPY/MODIFY/REPRODUCE/RE-DISTRIBUTE THIS PROGRAM, YOU MUST INCLUDE A COPY OF THE LICENSE
+
+Author Name: Kiet Pham
+Author Contact: kiet.riley2005@gmail.com
+Discord: CaptainVietnam6#7932
+Discord Server: https://discord.gg/3z76p8H5yj
+GitHub: https://github.com/CaptainVietnam6
+Instagram: @itz_kietttttttttt
+Program Status: ACTIVE, FINALISED
+'''
+
 #imports related to discord or discord packages
 import discord
 from discord.ext import commands
