fcbbdbfa80481413954a50ebf4f3f81e3640ebe8
Kiet Pham <>
2021-04-23 23:31:52 +0000

    updated notes, added token secret

--- BOT_TOKEN.py
+++ BOT_TOKEN.py
@@ -1,2 +1,3 @@
 #this is the bot token and will be required for the IDE to connect with discord's API
-BOT_TOKEN = "NzQ5MjAwOTU3OTM3NTQ5MzMy.X0ohww.LJokpSoPNLptYo1DWbsPpbbcsd4"
+import os
+BOT_TOKEN = os.environ["BOT_TOKEN_SECRET"]
